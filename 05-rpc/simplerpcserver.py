d98633ebb7ec7348c5d52b0c114c1c0dc7c2ab91
bhawiyuga <adhitya.bhawiyuga@gmail.com>
2020-10-22 13:54:15 +0700

    rpc bug fix

--- 05-rpc/simplerpcserver.py
+++ 05-rpc/simplerpcserver.py
@@ -3,7 +3,9 @@ import zerorpc
 class HelloRPC(object):
     def hello(self, name):
         return "Hello, %s" % name
-
-s = zerorpc.Server(HelloRPC())
-s.bind("tcp://0.0.0.0:4242")
-s.run()
+try :
+    s = zerorpc.Server(HelloRPC())
+    s.bind("tcp://0.0.0.0:4242")
+    s.run()
+except KeyboardInterrupt:
+    print("Keluar")
